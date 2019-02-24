7a379f55598df7721bbdac335445ee7fe6d1e403
kat! <theliteratecondition@gmail.com>
2019-02-24 17:52:40 -0600

    add logging and related to BookDao

--- src/main/java/edu/matc/persistence/BookDao.java
+++ src/main/java/edu/matc/persistence/BookDao.java
@@ -3,13 +3,17 @@ package edu.matc.persistence;
 import edu.matc.entity.Book;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
- * TODO At minimum create methods to create, read, update, and delete Books.
- * TODO a logger is used here, be sure to set up a log4j2.properties file
+ * The type BookDAO for the Book class.
  */
 public class BookDao {
 
@@ -20,8 +24,11 @@ public class BookDao {
      * Get book by id
      */
     public Book getById(int id) {
-
-        return null;
+        logger.info("In getById() using id {}", id);
+        Session session = sessionFactory.openSession();
+        Book book = session.get(Book.class, id);
+        session.close();
+        return book;
     }
 
     /**
@@ -29,15 +36,27 @@ public class BookDao {
      * @param book  Book to be inserted or updated
      */
     public void saveOrUpdate(Book book) {
-
+        logger.info("In saveOrUpdate() with Book {}", book);
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.saveOrUpdate(book);
+        transaction.commit();
+        session.close();
     }
 
     /**
-     * update Book
-     * @param book  Book to be inserted or updated
+     * insert Book
+     * @param book  Book to be inserted
      */
     public int insert(Book book) {
-        return 0;
+        logger.info("In insert() with Book {}", book);
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        id = (int)session.save(book);
+        transaction.commit();
+        session.close();
+        return id;
     }
 
     /**
@@ -45,19 +64,27 @@ public class BookDao {
      * @param book Book to be deleted
      */
     public void delete(Book book) {
-
+        logger.info("In delete() for Book {}", book);
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(book);
+        transaction.commit();
+        session.close();
     }
-
 
     /** Return a list of all books
      *
      * @return All books
      */
     public List<Book> getAll() {
-
-        return null;
+        logger.info("In getAll()");
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Book> query = builder.createQuery(Book.class);
+        Root<Book> root = query.from(Book.class);
+        List<Book> books = session.createQuery(query).getResultList();
+        session.close();
+        return books;
     }
-
-
 
 }
