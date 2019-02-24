38ea1257d545b4f7b38833cd1f46464c9c5a2fa7
kat! <theliteratecondition@gmail.com>
2019-02-24 16:54:13 -0600

    annotating book class

--- src/main/java/edu/matc/entity/Book.java
+++ src/main/java/edu/matc/entity/Book.java
@@ -3,69 +3,134 @@ package edu.matc.entity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+
 /**
- *
- * TODO add hibernate annotations
- * TODO Javadoc comments
+ * Represents a book object in the Book Seller application
+ * @author kRohrmeier
  */
 @Entity(name = "Book")
 @Table(name = "book")
 public class Book {
     private String title;
     private String author;
-    private int id;
     private String isbn;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+    @Column(name = "publication_year")
     private int publicationYear;
 
+    /**
+     * Instantiates a new Book.
+     */
     public Book() {
     }
 
-
-    public Book(String title, String author, int id, String isbn, int publicationYear) {
+    /**
+     * Instantiates a new Book.
+     *
+     * @param title           the title
+     * @param author          the author
+     * @param isbn            the isbn
+     * @param publicationYear the publication year
+     */
+    public Book(String title, String author, String isbn, int publicationYear) {
         this.title = title;
         this.author = author;
-        this.id = id;
         this.isbn = isbn;
         this.publicationYear = publicationYear;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     * Gets title.
+     *
+     * @return the title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets title.
+     *
+     * @param title the title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Gets publication year.
+     *
+     * @return the publication year
+     */
     public int getPublicationYear() {
         return publicationYear;
     }
 
+    /**
+     * Sets publication year.
+     *
+     * @param publicationYear the publication year
+     */
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
 
+    /**
+     * Gets author.
+     *
+     * @return the author
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Sets author.
+     *
+     * @param author the author
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    /**
+     * Gets isbn.
+     *
+     * @return the isbn
+     */
     public String getIsbn() {
         return isbn;
     }
 
+    /**
+     * Sets isbn.
+     *
+     * @param isbn the isbn
+     */
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", publicationYear=" + publicationYear +
+                '}';
     }
 }
