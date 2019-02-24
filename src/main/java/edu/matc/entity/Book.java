838bc35d162e99bf5bff323008f4dde73135ddd0
kat! <theliteratecondition@gmail.com>
2019-02-24 15:34:36 -0600

    add annotations to book class

--- src/main/java/edu/matc/entity/Book.java
+++ src/main/java/edu/matc/entity/Book.java
@@ -1,16 +1,71 @@
 package edu.matc.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 /**
- * TODO At minimum, complete the following
- * TODO create no-arg constructor
- * TODO create getters and setters
+ *
  * TODO add hibernate annotations
  * TODO Javadoc comments
  */
+@Entity(name = "Book")
+@Table(name = "book")
 public class Book {
     private String title;
     private String author;
     private int id;
     private String isbn;
     private int publicationYear;
+
+    public Book() {
+    }
+
+
+    public Book(String title, String author, int id, String isbn, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
