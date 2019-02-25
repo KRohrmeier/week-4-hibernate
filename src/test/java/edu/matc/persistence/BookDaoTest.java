a0a0d51bec699daea8c58490cea4b22da4a212c7
kat! <theliteratecondition@gmail.com>
2019-02-24 18:51:07 -0600

    add db and testing

--- src/test/java/edu/matc/persistence/BookDaoTest.java
+++ src/test/java/edu/matc/persistence/BookDaoTest.java
@@ -1,36 +1,55 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.Book;
+import edu.matc.test.util.Database;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // TODO add unit tests to achieve 100% code coverage of your dao.
 // TODO You should run these tests repeatedly, in any order, and they should all pass every time!
 // TODO Javadoc comments
 class BookDaoTest {
+    BookDao dao;
 
     @BeforeEach
     void setUp() {
+        dao = new BookDao();
+        // set up db afresh
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
     }
 
     @Test
-    void getById() {
+    void getByIdSuccess() {
+        Book returnedBook = dao.getById(2);
+        assertEquals("Beginning Hibernate", returnedBook.getTitle());
+        assertEquals("Joseph Ottinger", returnedBook.getAuthor());
+        assertEquals("978-1-4842-2319-2", returnedBook.getIsbn());
+        assertEquals("2016", returnedBook.getPublicationYear());
     }
 
     @Test
-    void saveOrUpdate() {
+    void saveOrUpdateSuccess() {
+        int id = 1;
+        String newTitle = "All Java All the Time";
+        Book bookToUpdate = dao.getById(id);
+        bookToUpdate.setTitle(newTitle);
+        dao.saveOrUpdate(bookToUpdate);
+        Book returnedBook = dao.getById(id);
+        assertEquals(newTitle, returnedBook.getTitle());
     }
 
     @Test
-    void insert() {
+    void insertSuccess() {
     }
 
     @Test
-    void delete() {
+    void deleteSuccess() {
     }
 
     @Test
-    void getAll() {
+    void getAllSuccess() {
     }
 }
